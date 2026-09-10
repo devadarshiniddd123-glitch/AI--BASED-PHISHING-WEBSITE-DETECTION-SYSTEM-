4ba914d456ae30831a6b8042225358017460d968
devadarshiniddd123-glitch <devadarshini.ddd123@gmail.com>
2026-09-10 20:53:15 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
